c09b28945f7777ceadeb322937f072869e8fd098
tmortred <tmortred112@gmail.com>
2022-09-15 17:14:16 +0800

    update access control interface

--- contracts/interfaces/ICyberSpawnAccessControl.sol
+++ contracts/interfaces/ICyberSpawnAccessControl.sol
@@ -5,4 +5,6 @@ pragma solidity ^0.8.0;
 interface ICyberSpawnAccessControl {
   function hasAdminRole(address account) external view returns (bool);
   function hasSpawnerRole(address account) external view returns (bool);
+  function hasGovernanceRole(address account) external view returns (bool);
+  function hasGameRole(address account) external view returns (bool);
 }
